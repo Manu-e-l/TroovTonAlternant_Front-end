cfbffaaa1b9e830a2c4138cd7ae2134a7adf728e
Manuel Elhadide <manuel.elhadide@gmail.com>
2022-11-11 20:04:09 +0100

    Init Nuxt

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+    root: true,
+    env: {
+        browser: true,
+        node: true
+    },
+    parserOptions: {
+        parser: "@babel/eslint-parser",
+        requireConfigFile: false
+    },
+    extends: [
+        "@nuxtjs",
+        "plugin:nuxt/recommended"
+    ],
+    plugins: [
+    ],
+    // add your custom rules here
+    rules: {
+        indent: [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        quotes: [
+            "error",
+            "double"
+        ],
+        semi: [
+            "error",
+            "always"
+        ]
+    }
+};
